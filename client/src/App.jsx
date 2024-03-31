d1dd52daf8baf83f4894b4d137409d35c80889ec
https://github.com/NeyuD96155 <phuoc10a12az@gmail.com>
2024-03-31 16:55:05 +0700

    setup route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,25 @@
-
+import{
+  BrowserRouter, Route, Routes}from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Dashboard from "./pages/Dashboard"
+import Projects from "./pages/Projects"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+    <Routes>
+<Route path="/" element ={<Home/>} />
+<Route path="/about" element ={<About/>} />
+<Route path="/contact" element ={<Contact/>} />
+<Route path="dashboard" element ={<Dashboard/>} />
+<Route path="project" element ={<Projects/>} />
+<Route path="/login" element ={<Login/>} />
+<Route path="/register" element ={<Register/>} />
+    </Routes>
+    </BrowserRouter>
+  )
 }
