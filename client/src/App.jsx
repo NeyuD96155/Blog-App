dee987d6eadd6e7a2d7b2ffb0355edfc0fc87694
https://github.com/NeyuD96155 <phuoc10a12az@gmail.com>
2024-04-01 12:24:33 +0700

    Create the header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,26 @@
-import{
-  BrowserRouter, Route, Routes}from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Contact from "./pages/Contact"
-import Dashboard from "./pages/Dashboard"
-import Projects from "./pages/Projects"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Routes>
-<Route path="/" element ={<Home/>} />
-<Route path="/about" element ={<About/>} />
-<Route path="/contact" element ={<Contact/>} />
-<Route path="dashboard" element ={<Dashboard/>} />
-<Route path="project" element ={<Projects/>} />
-<Route path="/login" element ={<Login/>} />
-<Route path="/register" element ={<Register/>} />
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="project" element={<Projects />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,54 @@
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
+export default function Header() {
+  const path = useLocation().pathname;
+
+  return (
+    <Navbar className="border-b-2">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm::text-xl font-semibold dark:text-white"
+      >
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+          Mint
+        </span>
+        Blog
+      </Link>
+      <form>
+        <TextInput
+          placeholder="Search"
+          className="hidden lg:inline"
+          type="text"
+          name="search"
+          id="search"
+          rightIcon={AiOutlineSearch}
+        />
+      </form>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+          <FaMoon />
+        </Button>
+        <Link to="/login">
+          <Button gradientDuoTone="purpleToBlue">Login</Button>
+        </Link>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/project"} as={"div"}>
+          <Link to="/project">Project</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
