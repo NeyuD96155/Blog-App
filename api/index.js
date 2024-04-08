ffd9feb9ae0098017bd206d0176cfd933f7d78f0
https://github.com/NeyuD96155 <phuoc10a12az@gmail.com>
2024-04-08 16:58:00 +0700

    auth controller 7 auth route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
 import userRoutes from "./routes/user.route.js"
+import authRoutes from "./routes/auth.route.js"
 dotenv.config();
 mongoose
   .connect(
@@ -19,3 +20,4 @@ app.listen(3000, () => {
 });
 
 app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
